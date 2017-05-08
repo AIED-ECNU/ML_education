e47890a8cb820424571ba529c4fd9cb95b954573
longhuiqiu <649053201@qq.com>
2017-05-08 16:36:29 +0800

    17.5.8

---
+++ Text_process.py
@@ -0,0 +1,23 @@
+# 导入包
+
+
+
+# 将pdf文件转化为txt文件。输入:pdf文件，得到txt文件
+def Pdf_to_txt():
+    pass
+
+# 清理txt文本语料。输入：txt文本，得到只有中字符和句中标点符号的txt文档
+def clearn_txt():
+    pass
+
+
+# 输入清理后的文本文件，对中文句子分词，并去除停顿词，其中停顿词和标点符号,输出分好词的txt
+def word_segment():
+
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ word2vec.py
@@ -0,0 +1,22 @@
+#导入包
+from Text_process import main
+
+
+# 输入分好词汇文本语料，返回词向量
+def word2vec():
+    pass
+
+
+
+
+
+# 输入相似词，在词向量中查找相似词汇，输出相似词汇，输出格式为：key：value
+def similar_word():
+
+    pass
+
+if __name__ == '__main__':
+
+    word_vec=word2vec()
+
+    similar_word()
