c1ce808f857c4f0abe81fadc78281e86bfc72d43
shirleysr <shirui1203@qq.com>
2017-05-11 19:03:19 +0800

    实现pdf转化为txt

--- Text_process.py
+++ Text_process.py
@@ -1,10 +1,35 @@
+# 安装pdfminer包：进入pdfminer3k-1.3.1文件下的命令窗口，输入python setup.py install
 # 导入包
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import io,os,re
 
-
-
-# 将pdf文件转化为txt文件。输入:pdf文件，得到txt文件
-def Pdf_to_txt():
-    pass
+# 将pdf文件转化为txt文件。输入:pdf文件夹，得到txt文件output.txt
+def Pdf_to_txt(fileDir):
+    codec = 'utf-8'
+    outfile="output.txt"
+    files = os.listdir(fileDir)
+    print (files)
+    for file in files:
+        manager = PDFResourceManager()
+        output = io.StringIO()
+        converter = TextConverter(manager, output, codec=codec, laparams=LAParams())
+        interpreter = PDFPageInterpreter(manager, converter)
+        filePath = fileDir + '\\' + file
+        infile = open(filePath, 'rb')
+        for page in PDFPage.get_pages(infile, check_extractable=True):
+           interpreter.process_page(page)
+           convertedPDF = output.getvalue()
+        print(convertedPDF)
+        with open('%s' % (outfile), 'a+b') as f:
+            f.write((os.path.basename(file)+convertedPDF).encode())#python3编码转换新方法“wb”,encode():http://pythoncentral.io/encoding-and-decoding-strings-in-python-3-x/
+        infile.close()
+        output.close()
+        converter.close()
+    print("Saved")
+    return 0
 
 # 清理txt文本语料。输入：txt文本，得到只有中字符和句中标点符号的txt文档
 def clearn_txt():
