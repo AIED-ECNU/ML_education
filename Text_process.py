738b30f68622d07dcced4bf05a0d9174a4236297
icecreamtoo <2249253421@qq.com>
2017-05-11 17:39:57 +0800

    分词部分
    简单的分词部分代码

--- Text_process.py
+++ Text_process.py
@@ -1,5 +1,6 @@
 # 导入包
-
+# encoding=utf-8
+import jieba
 
 
 # 将pdf文件转化为txt文件。输入:pdf文件，得到txt文件
@@ -13,6 +14,15 @@ def clearn_txt():
 
 # 输入清理后的文本文件，对中文句子分词，并去除停顿词，其中停顿词和标点符号,输出分好词的txt
 def word_segment():
+
+    seg_list = jieba.cut("戴尔公司将于５月１８日公布第一季度的财报。", cut_all=True)
+    print("Full Mode:", "/ ".join(seg_list))  # 全模式
+    seg_list = jieba.cut("我来到u北京清华大学", cut_all=False)
+    print("Default Mode:", "/ ".join(seg_list))  # 精确模式
+    seg_list = jieba.cut("戴尔公司将于５月１８日公布第一季度的财报。")  # 默认是精确模式
+    print(", ".join(seg_list))
+    seg_list = jieba.cut_for_search("你们好，市场竞争我是涂斌琴")  # 搜索引擎模式
+    print(", ".join(seg_list))
 
     pass
 
