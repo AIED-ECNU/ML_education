9312d45c17ae0c33d87cf5ef6226ffbe65a043d1
icecreamtoo <2249253421@qq.com>
2017-05-22 19:24:26 +0800

    分词模块完成
    word_initial.txt是文本材料

--- Text_process.py
+++ Text_process.py
@@ -1,6 +1,6 @@
 # 导入包
 # encoding=utf-8
-import jieba
+import jieba.analyse  # 导入结巴分词包
 
 
 # 将pdf文件转化为txt文件。输入:pdf文件，得到txt文件
@@ -15,16 +15,18 @@ def clearn_txt():
 # 输入清理后的文本文件，对中文句子分词，并去除停顿词，其中停顿词和标点符号,输出分好词的txt
 def word_segment():
 
-    seg_list = jieba.cut("戴尔公司将于５月１８日公布第一季度的财报。", cut_all=True)
-    print("Full Mode:", "/ ".join(seg_list))  # 全模式
-    seg_list = jieba.cut("我来到u北京清华大学", cut_all=False)
-    print("Default Mode:", "/ ".join(seg_list))  # 精确模式
-    seg_list = jieba.cut("戴尔公司将于５月１８日公布第一季度的财报。")  # 默认是精确模式
-    print(", ".join(seg_list))
-    seg_list = jieba.cut_for_search("你们好，市场竞争我是涂斌琴")  # 搜索引擎模式
-    print(", ".join(seg_list))
+    with open('stopword.txt', 'r') as f:
+        stopwords = f.read()  # 打开stopword（停用词）文件
+    with open('words_initial.txt', 'r') as f:
+        w = f.read()  # 读取要分词的文件
+        seg = jieba.cut_for_search(w)  # 进行分词
+        final = []  # 设置一个变量
+        for seg_list in seg:
+            if seg_list not in stopwords:
+                final.append(seg_list)  # 对第一次分词的结果进行处理，去掉停用词
+    seg_final = " ".join(final)  # 用空格符号分隔词语
 
-    pass
+
 
 def main():
     pass
