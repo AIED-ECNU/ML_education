e52f0c1756b9d6aeb74d4f2a38177caec67bceb8
shirleysr <shirui1203@qq.com>
2017-06-17 19:54:35 +0800

    txt cleaned

--- Text_process.py
+++ Text_process.py
@@ -7,25 +7,47 @@ from pdfminer.pdfpage import PDFPage
 import io,os,re
 
 # 将pdf文件转化为txt文件。输入:pdf文件夹，得到txt文件output.txt
-def Pdf_to_txt(fileDir,outfile):
+def pdf_to_txt(filelist):
     codec = 'utf-8'
-    files = os.listdir(fileDir)
-    for file in files:
-        manager = PDFResourceManager()
-        output = io.StringIO()
-        converter = TextConverter(manager, output, codec=codec, laparams=LAParams())
-        interpreter = PDFPageInterpreter(manager, converter)
-        filePath = fileDir + '\\' + file
-        with open(filePath, 'rb') as infile:
-         for page in PDFPage.get_pages(infile, check_extractable=True):
-            interpreter.process_page(page)
-            convertedPDF = output.getvalue()
-         print(convertedPDF)
-        # python3编码转换新方法“wb”,encode():http://pythoncentral.io/encoding-and-decoding-strings-in-python-3-x/
-        with open('%s' % (outfile), 'a+b') as f:
-            f.write(convertedPDF.encode())
-        output.close()
-        converter.close()
+    mylist=dict.fromkeys(['title', 'year'], ('unknow'))
+    with open (filelist,'r') as f:
+        i=f.read()
+        files = os.listdir(fileDir)
+        p1 = re.compile(r'(?<=%T ).+(?=' ')')#%T开头的字符串表示标题
+        p2 = re.compile(r'(?<=%D ).+(?=' ')')#%D开头的字符串表示时间
+        title=re.findall(p1,i)
+        year=re.findall(p2,i)
+        Dict=dict(zip(title,year))
+        print(Dict)
+        j=1
+    for i in Dict.keys():
+        #根据key值找文件
+        filePath='test\\files1\\'+i+'.pdf'
+        if os.path.exists(filePath):
+          manager = PDFResourceManager()
+          output = io.StringIO()
+          converter = TextConverter(manager, output, codec=codec, laparams=LAParams())
+          interpreter = PDFPageInterpreter(manager, converter)
+          f = open(outfile, 'a+b')
+          with open(filePath, 'rb') as infile:
+               content = []  # 定义一个数组变量用于暂存数据i
+               content.append('###'+i+'$'+Dict[i] +'$'+'###')#将标题用##符号包围起来
+               for page in PDFPage.get_pages(infile, check_extractable=True):
+                   interpreter.process_page(page)
+                   convertedPDF = output.getvalue()
+               print(convertedPDF)
+            # python3编码转换新方法“wb”,encode():http://pythoncentral.io/encoding-and-decoding-strings-in-python-3-x/
+               content.append(convertedPDF)
+
+          with open('%s' % (outfile), 'wb') as f:
+             f.write(''.join(content).encode())
+             print("-----------------------------------完成文章"+str(j)+"的pdf转化--------------------------------------")
+             j=j+1
+          output.close()
+          converter.close()
+          f.close()
+        else:
+            continue
     print("-----------------------------------完成pdf转化至txt--------------------------------------")
     return 0
 
@@ -34,11 +56,11 @@ def clean_txt(outfile):
     with open(outfile, 'rb')as f:
         content = f.read().decode('utf-8')
     # p定义了要挑选出的内容，其中\u4e00-\u9fff为中文字符的Unicode编码区间，\u3002\uFF0C分别表示句号与逗号
-    p = re.compile(u"[\u4e00-\u9fff+\u3002\uFF0C]")
+    p = re.compile(r'(?<=##)\S.+(?=##)|[\u4e00-\u9fff+\u3002\uFF0C]')
     # x将找出的符合条件的内容列表连接在一起输出
     x = ''.join(re.findall(p, content))
-    # 删除x中连续出现的重复内容，这里对逗号进行了处理
-    final_result = re.sub(u"[\uFF0C]{2,}", "", x)
+    # 删除x中连续出现的重复内容，这里对逗号,句号，加号进行了处理
+    final_result = re.sub(u"[\uFF0C|\u3002|\u002B]{2,}", "", x)
     with open(cleaned_file, "w")as outfile:
         outfile.write(final_result)
     print(final_result)
@@ -57,8 +79,9 @@ def clean_txt(outfile):
 #     main()
 
 
-fileDir=u'test\\files'
+fileDir = u'test\\files1'
+filelist=u'test\\filelist1.txt'
 outfile = "test\output.txt"
 cleaned_file = "test\\txt_cleaned.txt"
-Pdf_to_txt(fileDir,outfile)
+pdf_to_txt(filelist)
 clean_txt(outfile)
